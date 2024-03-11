1a9d4af2708daf3f547f7bb23c9ac89b58ff67ce
Jerome <159206525+Jerome424715@users.noreply.github.com>
2024-03-11 14:14:48 +1100

    changes

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let myIntro = 15;
-let enterName = 15;
+let enterName = prompt("Welcome! Please Enter Your Name");
+let playerGuess;
+let playerFeedback = "test";
+loadDIV ();
 
-if (myIntro == enterName); {
-    alert (myIntro);
+
+
+funtion loadDIV () {
+    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1><H2>Jess ar nombar beteen wan and wan"
+    2/*+-document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV") 
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback
+
+}
+
+function enterNumber () {
+    playerGuess = promppt ("anta ar nombar")
 }
